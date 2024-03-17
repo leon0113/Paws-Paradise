7915e5f7274e485a50d7f1d3e4f248ba8093b4d4
Leon <tahjib.leon399@gmail.com>
2024-03-17 10:49:37 +0600

    vercel production

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -3,6 +3,6 @@ import { ReactNode } from "react";
 
 export default function H1({ className, children }: { className?: string, children?: ReactNode }) {
     return (
-        <h1 className={cn("font-medium text-2xl leading-6 text-white", className)}>{children}</h1>
+        <h1 className={cn("font-medium text-2xl leading-6", className)}>{children}</h1>
     )
 }
